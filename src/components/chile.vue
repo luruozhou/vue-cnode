516140a144eada30d13a7d837cdde6cb46a16229
luruozhou <louie.lu@zmlearn.com>
2017-01-23 09:30:04 +0800

    父子组件通信

---
+++ src/components/chile.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="hello">
+    <h1 @click="changeName">{{ msg }}</h1>
+    <h1>{{ uName }}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'child',
+    data: function () {
+      return {
+
+      }
+    },
+    props:['msg','uName'],
+    methods:{
+      changeName:function () {
+        console.log(this.$emit);
+        this.$emit("changemalide",'aaa',1)
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  li {
+
+  a {
+    color: #f00;
+  }
+
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,19 +1,24 @@
 <template>
   <div class="hello">
-    <span class="title">Home</span>
+    <span class="title" @click="changeName">Home</span>
     <span v-on:click="toUser">点击去用户中心</span>
-    <ol>
-      <li v-for="article in artileList">
-        {{ article.title}}
-      </li>
-    </ol>
+    <achild msg="123" :uName="userName" v-on:changemalide="change2"></achild>
+    <!--<ol>-->
+      <!--<li v-for="article in artileList">-->
+        <!--{{ article.title}}-->
+      <!--</li>-->
+    <!--</ol>-->
   </div>
 </template>
 
 <script>
   import fetchData from '../util/fetchData';
+  import achild from '../components/chile.vue';
   export default {
     name: 'home',
+    components:{
+      achild
+    },
     data: function () {
       return {
         userName: 'lijingwen',
@@ -23,6 +28,13 @@
     methods: {
       toUser: function (event) {
         Router.push({path: '/user'})
+      },
+      changeName:function () {
+        this.userName ="luruozhou"
+      },
+      change2:function (e,num) {
+        console.log(e,num)
+        this.userName='lijingwen2'
       }
     },
     created: function (e) {
@@ -50,5 +62,6 @@
 <style scoped lang="scss">
   .title {
     font-size: 2rem;
+    color:#f00
   }
 </style>
